49a999926975a065f4361705f18f911a557598d6
Ryan Christian De Guzman <rycdguzman@icloud.com>
2021-11-09 13:19:49 +0800

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Ryan Christian De Guzman on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
